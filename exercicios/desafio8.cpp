36714e3065fbf78d6c2ec9a623e011d023ce4fec
Roberto Carlos Santos da Silva <118410010+Roberto-40K@users.noreply.github.com>
2022-12-12 19:02:47 -0300

    Update desafio8.cpp

--- exercicios/desafio8.cpp
+++ exercicios/desafio8.cpp
@@ -3,7 +3,8 @@
 using namespace std;
 
 /*
-Crie um Algoritmo em C++, que o usuário informa o tamanho de um vetor, e seus elementos. O Algoritmo deve imprimir este vetor e logo após, perguntar ao usuário se ele quer imprimir outro vetor. Utilize o delete após a impressão do vetor.
+Crie um Algoritmo em C++, que o usuário informa o tamanho de um vetor, e seus elementos. O Algoritmo deve imprimir este vetor e logo após, perguntar ao usuário
+se ele quer imprimir outro vetor. Utilize o delete após a impressão do vetor.
 */
 
 int main() {
